4ea95e7272a868cb07ccbef804f1fda08b7312e6
Hrishikesh <157392774+HrishiSwant@users.noreply.github.com>
2026-01-22 19:21:47 +0530

    Delete frontend/src/FrostBackground.js

--- frontend/src/FrostBackground.js
+++
@@ -1,9 +0,0 @@
-import Spline from "@splinetool/react-spline";
-
-export default function FrostBackground() {
-  return (
-    <div style={{ position: "fixed", inset: 0, zIndex: -2 }}>
-      <Spline scene="https://prod.spline.design/UemxkLFlofec3ccZ/scene.splinecode" />
-    </div>
-  );
-}
