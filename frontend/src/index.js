51cd7235c68338aaf1aa899418dbca5f19b6d1b3
Hrishikesh <157392774+HrishiSwant@users.noreply.github.com>
2026-01-22 19:26:11 +0530

    Remove FrostBackground from index.js
    Removed FrostBackground component from rendering.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,15 +3,11 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import FrostBackground from "./FrostBackground";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    {/* 3D Cyber Background */}
-    <FrostBackground />
-
     {/* Dark overlay for readability */}
     <div className="frost-overlay"></div>
 
