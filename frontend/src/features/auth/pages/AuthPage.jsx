426a46b9d32ea033fec4732e351801ab90d772c1
Hrishikesh <157392774+HrishiSwant@users.noreply.github.com>
2026-08-05 21:27:31 +0530

    Rename Authage component to AuthPage

--- frontend/src/features/auth/pages/AuthPage.jsx
+++ frontend/src/features/auth/pages/AuthPage.jsx
@@ -3,7 +3,7 @@ import { SignIn, SignUp } from "@clerk/clerk-react";
 import { useState } from "react";
 import { ArrowLeft } from "lucide-react";
 
-export default function Authage({ goBack, theme }) {
+export default function AuthPage({ goBack, theme }) {
   const [mode, setMode] = useState("sign-in"); // "sign-in" or "sign-up"
 
   return (
