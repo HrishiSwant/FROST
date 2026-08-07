c1418a4e203d39cf12bb08f8a6fda437f3148d8f
Hrishikesh <157392774+HrishiSwant@users.noreply.github.com>
2026-08-07 19:33:12 +0530

    Add StatsGrid and QuickActions to DashboardPage

--- frontend/src/features/dashboard/pages/DashboardPage.jsx
+++ frontend/src/features/dashboard/pages/DashboardPage.jsx
@@ -1,12 +1,17 @@
-import React from "react";
-
 import AppShell from "../../../components/layouts/AppShell/AppShell";
+
 import DashboardHeader from "../components/DashboardHeader";
+import StatsGrid from "../components/StatsGrid";
+import QuickActions from "../components/QuickActions";
 
 export default function DashboardPage() {
   return (
     <AppShell>
       <DashboardHeader />
+
+      <StatsGrid />
+
+      <QuickActions />
     </AppShell>
   );
 }
