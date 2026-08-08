d5198d5ff9b99c5c206a8c88baa14776a1177fdc
Hrishikesh <157392774+HrishiSwant@users.noreply.github.com>
2026-08-09 03:03:18 +0530

    Add AboutFrost route to AppRouter

--- frontend/src/core/router/AppRouter.jsx
+++ frontend/src/core/router/AppRouter.jsx
@@ -8,24 +8,38 @@ import {
 
 import LandingPage from "../../features/dashboard/pages/LandingPage";
 import DashboardPage from "../../features/dashboard/pages/DashboardPage";
+import AboutFrost from "../../features/dashboard/pages/AboutFrost";
 import Deepfake from "../../pages/Deepfake";
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
+        {/* Landing */}
+        <Route
+          path="/"
+          element={<LandingPage />}
+        />
 
+        {/* Dashboard */}
         <Route
           path="/dashboard"
           element={<DashboardPage />}
         />
 
+        {/* About FROST */}
+        <Route
+          path="/about"
+          element={<AboutFrost />}
+        />
+
+        {/* Deepfake Detection */}
         <Route
           path="/deepfake"
           element={<Deepfake />}
         />
 
+        {/* Unknown routes */}
         <Route
           path="*"
           element={<Navigate to="/" replace />}
