138a537bab21f880b09540515cc11039216ea847
Hrishikesh <157392774+HrishiSwant@users.noreply.github.com>
2026-08-08 23:09:04 +0530

    Add new routes for Deepfake, Fakenews, and Admin

--- frontend/src/core/router/AppRouter.jsx
+++ frontend/src/core/router/AppRouter.jsx
@@ -9,13 +9,39 @@ import {
 import LandingPage from "../../features/dashboard/pages/LandingPage";
 import DashboardPage from "../../features/dashboard/pages/DashboardPage";
 
+import Deepfake from "../../pages/Deepfake";
+import Fakenews from "../../pages/Fakenews";
+import AdminDashboard from "../../pages/AdminDashboard";
+import AboutFrost from "../../features/dashboard/pages/AboutFrost";
+
 export default function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
+
+        {/* Landing / Intro */}
         <Route path="/" element={<LandingPage />} />
+
+        {/* Main Dashboard */}
         <Route path="/dashboard" element={<DashboardPage />} />
-        <Route path="*" element={<Navigate to="/" />} />
+
+        {/* Security Modules */}
+        <Route path="/dashboard/deepfake" element={<Deepfake />} />
+
+        <Route path="/dashboard/fake-news" element={<Fakenews />} />
+
+        {/* Information */}
+        <Route path="/about" element={<AboutFrost />} />
+
+        {/* Admin */}
+        <Route path="/admin" element={<AdminDashboard />} />
+
+        {/* Unknown route */}
+        <Route
+          path="*"
+          element={<Navigate to="/" replace />}
+        />
+
       </Routes>
     </BrowserRouter>
   );
