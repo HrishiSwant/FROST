772fc0bc9b15d6acfe149c8e0e7921bfb81b225b
Hrishikesh <157392774+HrishiSwant@users.noreply.github.com>
2026-08-09 19:13:53 +0530

    Remove unused components from DashboardPage
    Removed DashboardHeader and QuickActions components from DashboardPage.

--- frontend/src/features/dashboard/pages/DashboardPage.jsx
+++ frontend/src/features/dashboard/pages/DashboardPage.jsx
@@ -1,17 +1,11 @@
 import AppShell from "../../../components/layouts/AppShell/AppShell";
 import ModuleGrid from "../components/ModuleGrid";
-import DashboardHeader from "../components/DashboardHeader";
 import StatsGrid from "../components/StatsGrid";
-import QuickActions from "../components/QuickActions";
 
 export default function DashboardPage() {
   return (
     <AppShell>
-      <DashboardHeader />
-
       <StatsGrid />
-
-      <QuickActions />
 
       <ModuleGrid />
     </AppShell>
