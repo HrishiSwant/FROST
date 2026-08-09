77b69ec26fe2a302e5ce8732884635a350462f6b
Hrishikesh <157392774+HrishiSwant@users.noreply.github.com>
2026-08-09 19:06:45 +0530

    Add PhoneIntelligencePage route to AppRouter

--- frontend/src/core/router/AppRouter.jsx
+++ frontend/src/core/router/AppRouter.jsx
@@ -9,23 +9,36 @@ import {
 import LandingPage from "../../features/dashboard/pages/LandingPage";
 import DashboardPage from "../../features/dashboard/pages/DashboardPage";
 import AboutFrost from "../../features/dashboard/pages/AboutFrost";
+
 import Deepfake from "../../pages/Deepfake";
 import Fakenews from "../../pages/Fakenews";
+
+import PhoneIntelligencePage from "../../features/phone-intelligence/pages/PhoneIntelligencePage";
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* Landing */}
         <Route path="/" element={<LandingPage />} />
 
+        {/* Dashboard */}
         <Route path="/dashboard" element={<DashboardPage />} />
 
+        {/* About */}
         <Route path="/about" element={<AboutFrost />} />
 
+        {/* Intelligence Modules */}
         <Route path="/deepfake" element={<Deepfake />} />
 
         <Route path="/news" element={<Fakenews />} />
 
+        <Route
+          path="/phone"
+          element={<PhoneIntelligencePage />}
+        />
+
+        {/* Unknown route */}
         <Route
           path="*"
           element={<Navigate to="/" replace />}
